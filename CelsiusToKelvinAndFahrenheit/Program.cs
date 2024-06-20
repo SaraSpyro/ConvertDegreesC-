b88ef29c42076a6a91bc62bbcbb1c646b92fda4c
SaraFarahani <67312912+SaraSpyro@users.noreply.github.com>
2024-06-20 20:55:27 +0330

    Initial commit

---
+++ CelsiusToKelvinAndFahrenheit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CelsiusToKelvinAndFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" Enter the amount of celsius: ");
+                byte celsius = Convert.ToByte(Console.ReadLine());
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($" Kelvin = { celsius + 273}");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($" Fahrenheit = {celsius * 18 / 10 + 32}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
